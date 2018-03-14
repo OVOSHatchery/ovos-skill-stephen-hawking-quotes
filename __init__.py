2192175922264075542e4a77bcd85274fd8a2061
jarbasai <jarbasai@mailfence.com>
2018-03-14 17:53:44 +0000

    voice tuning

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ from mycroft.tts.espeak_tts import ESpeak
 class StephenHawkingSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-        self.espeak = ESpeak("english-us", "m1")
+        self.espeak = ESpeak("en-uk", "m2")
 
     def initialize(self):
         self.espeak.init(self.emitter)
@@ -21,13 +21,13 @@ class StephenHawkingSkill(MycroftSkill):
         utterance = self.dialog_renderer.render("quote", {})
         self.hawking_speak(utterance)
 
-    @intent_handler(IntentBuilder("StephenHawkingbirth").require(
+    @intent_handler(IntentBuilder("StephenHawkingBirth").require(
         'StephenHawking').require(
         'birth'))
     def handle_birth(self, message):
         self.speak_dialog('birth')
 
-    @intent_handler(IntentBuilder("StephenHawkingQuote").require(
+    @intent_handler(IntentBuilder("StephenHawkingDeath").require(
         'StephenHawking').require(
         'death'))
     def handle_death(self, message):
