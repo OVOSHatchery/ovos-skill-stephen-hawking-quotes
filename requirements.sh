49a4fd50a15138764733c7ad26937c3c5553f260
Nhoya <Nhoya@users.noreply.github.com>
2018-03-15 11:33:56 +0100

    Improved distro detection

--- requirements.sh
+++ requirements.sh
@@ -3,12 +3,12 @@
 # in most cases.
 
 #detect distribution using lsb_release (may be replaced parsing /etc/*release)
-dist=$(lsb_release -d |awk '{print$2}')
+dist=$(lsb_release -ds)
 
 #setting dependencies and package manager in relation to the distribution
-if [ "$dist"  == "Arch"  ]; then
+if [ "$dist"  == "\"Arch Linux\""  ]; then
     pm="pacman -S"
-elif [ "$dist" ==  "Ubuntu" ] || [ "$dist" == "KDE" ] || [ "$dist" == "Debian" ]; then
+elif [[ "$dist" =~  "Ubuntu" ]] || [[ "$dist" =~ "Debian" ]]; then
     pm="apt install"
 fi
 dependencies=( espeak )
