4af498dfe7606e5287bda94844ca4f5376772a76
jarbasal <jarbasai@mailfence.com>
2020-06-08 23:16:52 +0100

    gui / big screen support

--- __init__.py
+++ __init__.py
@@ -1,18 +1,35 @@
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_handler
 from mycroft.tts.espeak_tts import ESpeak
+from os.path import join, dirname
 
 
-class StephenHawkingSkill(MycroftSkill):
+class StephenHawkingTributeSkill(MycroftSkill):
     def __init__(self):
         super().__init__()
-        self.espeak = ESpeak("en-uk", {"voice": "m2"})
+        try:
+            self.espeak = ESpeak("en-uk", {"voice": "m1"})
+        except:
+            self.espeak = None
 
     def initialize(self):
-        self.espeak.init(self.bus)
+        if self.espeak:
+            self.espeak.init(self.bus)
+        self.add_event('skill-stephen-hawking.jarbasskills.home', self.homepage)
+
+    def homepage(self):
+        utterance = """"
+My goal is simple. It is a complete understanding of the universe, why it is as it is and why it exists at all."
+
+-- Stephen Hawking, 1942 - 2018
+"""
+        self.gui.show_text(utterance)
 
     def hawking_speak(self, utterance):
-        self.espeak.execute(utterance)
+        if self.espeak:
+            self.espeak.execute(utterance)
+        else:
+            self.speak(utterance)
 
     @intent_handler(IntentBuilder("StephenHawkingQuote").require(
         'StephenHawking').require(
@@ -22,18 +39,30 @@ class StephenHawkingSkill(MycroftSkill):
         self.log.info("speak: " + utterance)
         self.hawking_speak(utterance)
 
+        self.gui.clear()
+        self.gui.show_image(join(dirname(__file__), "ui", "hawking.jpg"),
+                            caption=utterance, fill='PreserveAspectFit')
+
+
     @intent_handler(IntentBuilder("StephenHawkingBirth").require(
         'StephenHawking').require(
         'birth'))
     def handle_birth(self, message):
-        self.speak_dialog('birth')
+        utterance = self.dialog_renderer.render("birth", {})
+        self.speak_dialog(utterance)
+        self.gui.clear()
+        self.gui.show_image(join(dirname(__file__), "ui", "young_hawking.jpg"),
+                            caption=utterance, fill='PreserveAspectFit')
 
     @intent_handler(IntentBuilder("StephenHawkingDeath").require(
         'StephenHawking').require(
         'death'))
     def handle_death(self, message):
-        self.speak_dialog('death')
-
+        utterance = self.dialog_renderer.render("death", {})
+        self.speak_dialog(utterance)
+        self.gui.clear()
+        self.gui.show_image(join(dirname(__file__), "ui", "hawking.jpg"),
+                            caption=utterance, fill='PreserveAspectFit')
 
 def create_skill():
-    return StephenHawkingSkill()
+    return StephenHawkingTributeSkill()
